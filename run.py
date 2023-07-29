5139ad78a0be21da8f1ab0bae838f2fbe1a5dd6d
josipcodes <>
2023-07-29 14:09:47 +0000

    Add check_difference function to compare previous and the current choice

--- run.py
+++ run.py
@@ -21,16 +21,36 @@ def check_if_won(random_number, player_choice):
 
 def check_choice(random_number, player_choice):
     difference = 0
-    if bool(player_guesses) == False:
-        print("Cold")
-    elif bool(player_guesses):
-        smaller_number = min(random_number, player_choice)
-        bigger_number = max(random_number, player_choice)
-        difference = bigger_number - smaller_number
-        print(difference)
+    # if not bool(player_guesses):
+    #     print("Cold")
+    #     check_difference(random_number, player_choice)
+    # elif bool(player_guesses):
+    smaller_number = min(random_number, player_choice)
+    bigger_number = max(random_number, player_choice)
+    difference = bigger_number - smaller_number
     player_guesses.append(difference)
+    check_difference(player_guesses)
 
 
+def check_difference(player_guesses):
+    """
+    Function checks player's guesses.
+    If it's player's first guess, confirms it's not right.
+    If the last two guesses player made are equal length away from the result,
+    player is informed they haven't guessed the number.
+    If player's last guess is worse than the previous one, player is informed.
+    If player's last guess is better than the previous one, player is informed.
+    """
+    if len(player_guesses) == 1:
+        #todo: build logic
+        print("You made a guess...but it's wrong...")
+    else:
+        if player_guesses[-2] < player_guesses[-1]:
+            print("Colder...")
+        elif player_guesses[-2] == player_guesses[-1]:
+            print("Try again, still not right.")
+        else:
+            print("Warmer")
 
 
 while random_number != player_choice:
