34406550e35a89cdd96ab38c4ca9ab39fb2ac203
josipcodes <>
2023-08-01 16:15:56 +0000

    Fix line length, update all docstrings

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import random
-import math
-from pprint import pprint # is it needed?
+from pprint import pprint  # is it needed?
 from simple_term_menu import TerminalMenu
 # Copied from Love sandwiches project
 import gspread
@@ -22,7 +21,7 @@ SHEET = GSPREAD_CLIENT.open("hot-or-cold-scoreboard")
 
 def update_scoreboard(game_mode):
     """
-    Function pulls top 10 results depending on the difficulty played.
+    Function pulls top 10 results depending on the difficulty played:
     Function collects values from 2 columns.
     Values are zipped together and sorted by the int in second column.
     Function prints out top 10 results off the scoreboard
@@ -40,21 +39,26 @@ def update_scoreboard(game_mode):
         print(item)
 
 
-def scoreboard(game_mode): #review function naming
+def scoreboard(game_mode):  # review function naming
     """
-    Function obtains user's username and pushes it along with the amount of guesses to
+    Function obtains user's username,
+    pushes it along with the amount of guesses to
     the relevant worksheet depending on the difficulty played.
     Function calls update_scoreboard().
+    Alternatively, function thanks user for playing.
     """
     data = []
     while True:
-        scoreboard_confirm = input("Want to see if you make it onto the leaderboard? (y/n) ")
+        scoreboard_confirm = input(
+            "Want to see if you make it onto the leaderboard? (y/n) "
+        )
         print(scoreboard_confirm)
         if scoreboard_confirm.lower() == "y":
             username = input("Enter a preferred username: ")
             data.append(username)
             data.append(len(player_guesses) + 1)
-            print(f"Hi, {username}, let's see if you've scored a position on the leaderboard...")
+            print(f"Hi {username}, let's see " \
+            "if you've scored a position on the leaderboard...")
             worksheet = SHEET.worksheet(game_mode)
             worksheet.append_row(data)
             update_scoreboard(game_mode)
@@ -64,34 +68,36 @@ def scoreboard(game_mode): #review function naming
             print("That's ok, thank you for playing! \n")
             break
         else:
-            print(f"You have entered '{scoreboard_confirm}', which is not a valid command.")
+            print(f"You have entered '{scoreboard_confirm}'.")
+            print("This is not a valid command.")
             print("Please enter a valid command. \n")
 
 
 def check_if_won(random_number, player_choice, game_mode, difficulty):
     """
     Function compares player's guess with the computer's choice.
-    If the choice is correct, player won.
-    If the guess is wrong, check_choice function is called. #todo: Decide the hot/cold logic. 
+    If the choice is correct, player won:
+    Function informs the player - wording depends on the guess volume.
+    If the guess is wrong, check_choice function is called.
     """
     if random_number == player_choice:
         if len(player_guesses) != 0:
             win_statement = "attempts"
         else:
             win_statement = "attempt"
-        print("You won!") 
+        print("You won!")
         print(f"It took you {len(player_guesses) + 1} {win_statement}.\n")
         scoreboard(game_mode)
-    elif player_choice == None:
+    elif player_choice is None:
         pass
     else:
         check_choice(random_number, player_choice, difficulty)
 
 
-def check_choice(random_number, player_choice, difficulty): #investigate why this is called unnecessarily.
+def check_choice(random_number, player_choice, difficulty):
     """
+    Function calculates min and max between random_number and player_choice.
     Function calculates the difference between the bigger and smaller number.
-    Numbers are comprised of the computer's and player's choices.
     Difference is appended to player_guesses.
     check_difference function is called.
     """
@@ -109,71 +115,77 @@ def check_difference(player_guesses, difficulty):
     If it's player's first guess, confirms it's not right.
     If the last two guesses player made are equal length away from the result,
     player is informed they haven't guessed the number.
-    If player's last guess is worse than the previous one, player is informed.
-    If player's last guess is better than the previous one, player is informed.
+    If the last guess is worse than the previous one, player is informed.
+    If the last guess is better than the previous one, player is informed.
+    If the guess is within 10% of the random_number, player is informed.
     #need to add a note regarding multiple statements.
     """
     if player_guesses[-1] <= difficulty / 10:
         hot_statements = [
-        "Hot! Hot! Hot!...", 
-        "Sizzling!", 
-        "Global warming ahead, you're close!"
+            "Hot! Hot! Hot!...",
+            "Sizzling!",
+            "Global warming ahead, you're close!"
         ]
         statement_index = random.randint(0, 2)
         print(f"{hot_statements[statement_index]}")
-    elif len(player_guesses) == 1: #todo: investigate why this is triggered (early appending)
-        #todo: build logic
+    elif len(player_guesses) == 1:
         print(player_guesses)
         first_guess_statements = [
-        "You made a guess...but it's wrong...", 
-        "Hmmm, not quite right, try again.", 
-        "Wanna take another swing at this?"
+            "You made a guess...but it's wrong...",
+            "Hmmm, not quite right, try again.",
+            "Wanna take another swing at this?"
         ]
         statement_index = random.randint(0, 2)
         print(f"{first_guess_statements[statement_index]}")
     else:
         if player_guesses[-2] < player_guesses[-1]:
             colder_statements = [
-            "Colder...", 
-            "If you're trying to be bad at this, great job!", 
-            "Snowman alert!"
+                "Colder...",
+                "If you're trying to be bad at this, great job!",
+                "Snowman alert!"
             ]
             statement_index = random.randint(0, 2)
             print(f"{colder_statements[statement_index]}")
         elif player_guesses[-2] == player_guesses[-1]:
             same_difference_statements = [
-            "Hmmm, not warmer, nor colder, surprisingly.", 
-            "Yeah, so the answer is somewhere in between your previous guess and this one.", 
-            "Same temperature, seems like you like consistency.",
-            "April 25th, it's not too hot, not too cold. All you need is a light jacket."
+                "Hmmm, not warmer, nor colder, surprisingly.",
+                "Yeah, so the answer is somewhere in " \
+                "between your previous guess and this one.",
+                "Same temperature, seems like you like consistency.",
+                "April 25th, it's not too hot, not too cold. " \
+                "All you need is a light jacket."
             ]
             statement_index = random.randint(0, 3)
             print(f"{same_difference_statements[statement_index]}")
         else:
             warmer_statements = [
-            "Warmer...", 
-            "Temperature rising!", 
-            "Oooh, someone switched on the heating!"
+                "Warmer...",
+                "Temperature rising!",
+                "Oooh, someone switched on the heating!"
             ]
             statement_index = random.randint(0, 2)
             print(f"{warmer_statements[statement_index]}")
 
+
 player_guesses = []
+
 
 def run_game(difficulty, game_mode):
     """
     Function takes the difficulty level chosen and adjust parameters.
-    Function obtains a pseudo-random number between 1 and difficulty int.
+    It obtains a pseudo-random number between 1 and difficulty int.
     It asks for input from user as long as user doesn't guess the number.
+    Once input is received, validate_input is called.
+    When validation result is received back, 
+    input is converted into an integer and check_if_won is called.
 
     """
-
-    random_number = random.randint(1, difficulty) #todo: when menu is created, stop needs to depend on the game mode chosen. 
+    random_number = random.randint(1, difficulty)
     print(f"You need to guess a number between 1 and {difficulty}.")
-    print(random_number) #todo: delete later
+    print(random_number)  # todo: delete later
     player_choice = None
     validation = False
-    while validation == False:
+    while validation is False:
         player_choice = input("Your guess: ")
         if validate_input(player_choice, difficulty):
             player_choice = int(player_choice)
@@ -198,32 +210,42 @@ def validate_input(player_choice, difficulty):
             return True
         else:
             print(
-                f"Hold on, Katy Perry, you're guessing a number between 1 and {difficulty}. \n"
+                "Hold on, Katy Perry, " \
+                f"you're guessing a number between 1 and {difficulty}. \n"
                 )
             return False
 
 
-#at the moment copied from simple term menu, needs review
+# at the moment copied from simple term menu, needs review
 def menu():
+    """
+    Function creates a main menu and a difficulty menu.
+    """
     # Main menu options.
     main_options = [
-        "[1] New Game", 
-        "[2] About", 
-        "[3] Leaderboard", 
+        "[1] New Game",
+        "[2] About",
+        "[3] Leaderboard",
         "[4] Quit"]
 
     # Sub-menu options.
     difficulty_options = [
         "[1] Beginner",
-        "[2] Intermediate", 
+        "[2] Intermediate",
         "[3] Expert",
         "[4] Go back"]
-    main_menu = TerminalMenu(main_options, title = "Hot or Cold")
-    difficulty_menu = TerminalMenu(difficulty_options, title = "Difficulty level")
+    main_menu = TerminalMenu(
+        main_options,
+        title = "Hot or Cold"
+    )
+    difficulty_menu = TerminalMenu(
+        difficulty_options,
+        title = "Difficulty level"
+    )
     # main_menu_style = ("bg_red", "fg_yellow") #todo: need to check
     quit_game = False
 
-    while quit_game == False:
+    while quit_game is False:
         current_display = main_menu.show()
         user_choice = main_options[current_display]
 
@@ -233,7 +255,22 @@ def menu():
             quit_game = True
             current_display = main_menu.show()
         elif user_choice == "[2] About":
-            print("about")
+            print(
+                "This game was created for educational purposes " \
+                "only as a part of the course with Code Institute. \n"
+            )
+            print(
+                "The objective of the game is to guess the correct number. " \
+                "The player can choose the difficulty, and as a result " \
+                "the player will have to guess a number between " \
+                "1-10, 1-100 or 1-1000. \n"
+            )
+            print(
+                "Unlike the usual guessing games, this one won't help " \
+                "the player by saying if the number is higher or lower. " \
+                "Worry not, the player will be told if their number is... " \
+                "Well, hot! \n"
+            )
         elif user_choice == "[3] Leaderboard":
             print("leaderboard")
         elif user_choice == "[1] New Game":
@@ -255,17 +292,14 @@ def menu():
 
 def main():
     """
-    Function unpacks a tuple
+    Function unpacks a tuple.
+    It calls run_game if the game_status is True.
     """
     game_status, difficulty, game_mode = menu()
-    if game_status == True:
+    if game_status is True:
         print("if reached")
         run_game(difficulty, game_mode)
-    
+
 
 main()
 menu()
-
-
-
-
